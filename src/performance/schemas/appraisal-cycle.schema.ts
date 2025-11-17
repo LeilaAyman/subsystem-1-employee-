e7a324af10605ca385bcfc7d72bb3d6d5277a11d
LeilaAyman <mariamaymanaliomar@gmail.com>
2025-11-17 17:14:59 +0200

    integrations doen

--- src/performance/schemas/appraisal-cycle.schema.ts
+++ src/performance/schemas/appraisal-cycle.schema.ts
@@ -2,14 +2,19 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose, { HydratedDocument } from 'mongoose';
 import { AppraisalType } from '../enum/appraisal-type.enum';
 import { AppraisalStatus } from '../enum/appraisal-status.enum';
+
 export type AppraisalCycleDocument = HydratedDocument<AppraisalCycle>;
 
 @Schema({ timestamps: true })
 export class AppraisalCycle {
   @Prop({ required: true })
-  name: string; // "2025 Annual Appraisal"
+  name: string; // e.g. "2025 Annual Appraisal"
 
-  @Prop({ type: String, enum: AppraisalType, required: true })
+  @Prop({
+    type: String,
+    enum: AppraisalType,
+    required: true,
+  })
   type: AppraisalType;
 
   @Prop({ type: Date, required: true })
@@ -20,20 +25,20 @@ export class AppraisalCycle {
 
   @Prop({
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'AppraisalTemplate',
+    ref: 'PerformanceTemplate',   // FIXED HERE!!
     required: true,
   })
   template: mongoose.Types.ObjectId;
 
   @Prop({
-  type: String,
-  enum: AppraisalStatus,
-  default: AppraisalStatus.DRAFT,
-})
-status: AppraisalStatus;
+    type: String,
+    enum: AppraisalStatus,
+    default: AppraisalStatus.DRAFT,
+  })
+  status: AppraisalStatus;
 
   @Prop({ type: [String], default: [] })
-  includedOrgUnits?: string[]; // departments or units
+  includedOrgUnits?: string[]; // department IDs
 }
 
 export const AppraisalCycleSchema =
