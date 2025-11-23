feb2a5b2c0a106f3e7b54e7f6c8ea3ff94cc1799
LeilaAyman <mariamaymanaliomar@gmail.com>
2025-11-24 01:14:43 +0200

    k

--- src/employee-profile/employee-profile.service.ts
+++ src/employee-profile/employee-profile.service.ts
@@ -1,14 +1,4 @@
-// src/employee/employee.service.ts
 import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { Employee, EmployeeDocument } from './models/employee.schema';
-import { EmploymentStatus } from './enums/employment-status.enum';
 
 @Injectable()
-export class EmployeeProfileService {
-  constructor(
-    @InjectModel(Employee.name)
-    private readonly employeeModel: Model<EmployeeDocument>,
-  ) {}
-}
+export class EmployeeProfileService {}
