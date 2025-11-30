3f678b0e10fb523a4d3c1513d955911c2ede501b
LeilaAyman <mariamaymanaliomar@gmail.com>
2025-11-30 13:37:22 +0200

    some progress  ut stlill finishing up the employee

--- src/employee-profile/employee-profile.module.ts
+++ src/employee-profile/employee-profile.module.ts
@@ -1,41 +1,56 @@
-// src/employee-profile/employee-profile.module.ts
-
-import { Module, forwardRef } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-
 import { EmployeeProfileController } from './employee-profile.controller';
 import { EmployeeProfileService } from './employee-profile.service';
-
-// Schemas
-import { Employee, EmployeeSchema } from './models/employee.schema';
+import { EmployeeRoleService } from './services/employee-role.service';
+import { EmployeeCrudService } from './services/employee-crud.service';
+import { EmployeeSelfServiceService } from './services/employee-self-service.service';
+import { ChangeRequestService } from './services/change-request.service';
+import { FileUploadService } from './services/file-upload.service';
+import { HrAdminService } from './services/hr-admin.service';
+import { Candidate, CandidateSchema } from './models/candidate.schema';
 import {
-  EmployeeDocumentFile,
-  EmployeeDocumentSchema,
-} from './models/employee-document.schema';
+  EmployeeProfile,
+  EmployeeProfileSchema,
+} from './models/employee-profile.schema';
 import {
-  EmployeeChangeRequest,
-  EmployeeChangeRequestSchema,
-} from './models/employee-change-request.schema';
+  EmployeeSystemRole,
+  EmployeeSystemRoleSchema,
+} from './models/employee-system-role.schema';
+import {
+  EmployeeProfileChangeRequest,
+  EmployeeProfileChangeRequestSchema,
+} from './models/ep-change-request.schema';
+import {
+  EmployeeQualification,
+  EmployeeQualificationSchema,
+} from './models/qualification.schema';
+import { PerformanceModule } from '../performance/performance.module';
 
-// Imported Modules ( Integrations)
-import { OrgStructureModule } from 'src/org-structure/org-structure.module';
-import { PerformanceModule } from 'src/performance/performance.module';
-import {RecruitmentModule }from 'src/recruitment/recruitment.module';
 @Module({
   imports: [
     MongooseModule.forFeature([
-      { name: Employee.name, schema: EmployeeSchema },
-      { name: EmployeeDocumentFile.name, schema: EmployeeDocumentSchema },
-      { name: EmployeeChangeRequest.name, schema: EmployeeChangeRequestSchema },
-    ]),OrgStructureModule,PerformanceModule,RecruitmentModule
-
-  
-    
+      { name: Candidate.name, schema: CandidateSchema },
+      { name: EmployeeProfile.name, schema: EmployeeProfileSchema },
+      { name: EmployeeSystemRole.name, schema: EmployeeSystemRoleSchema },
+      {
+        name: EmployeeProfileChangeRequest.name,
+        schema: EmployeeProfileChangeRequestSchema,
+      },
+      { name: EmployeeQualification.name, schema: EmployeeQualificationSchema },
+    ]),
+    PerformanceModule,
   ],
-
   controllers: [EmployeeProfileController],
-  providers: [EmployeeProfileService],
-
+  providers: [
+    EmployeeProfileService,
+    EmployeeRoleService,
+    EmployeeCrudService,
+    EmployeeSelfServiceService,
+    ChangeRequestService,
+    FileUploadService,
+    HrAdminService,
+  ],
   exports: [EmployeeProfileService],
 })
 export class EmployeeProfileModule {}
